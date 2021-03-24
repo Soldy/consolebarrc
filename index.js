9f70f77e76ded38b79cecd7ae7c828200d2dcade
Soldy <4786022+Soldy@users.noreply.github.com>
2021-03-24 07:18:16 +0000

    loop remove
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const styler = new (require('consolestylerc')).base();
 
 const barrcBase=function(){
     this.init = function(inp){
-        return init(inp);
+        return _init(inp);
     }
     this.addLine = function(inp){
-        return addLine(inp);
+        return _addLine(inp);
     }
     this.update = function(inp){
-        return upDate(inp);
+        return _upDate(inp);
     }
     this.draw = function(bar){
-        return draw(bar);
+        return _draw(bar);
     }
-    let bars = {};
-    const init = function(inp){
+    let _bars = {};
+    const _init = function(inp){
         if(typeof inp.name === 'undefined')
             return false;
         if(typeof inp.max === 'undefined')
             inp.max = 10;
         if(typeof inp.lines === 'undefined')
             inp.lines = {};
-        bars[inp.name]={
+        _bars[inp.name]={
             size  : 10,
             max   : inp.max,
             lines : inp.lines
         };
         return true;
     };
-    const addLine = function(inp){
+    const _addLine = function(inp){
         if(
             (typeof inp.bar === 'undefined')||
-            (typeof bars[inp.bar] === 'undefined')||
+            (typeof _bars[inp.bar] === 'undefined')||
             (typeof inp.id  === 'undefined')||
             (typeof inp.title === 'undefined')
         )
@@ -45,70 +45,82 @@ const barrcBase=function(){
             (typeof inp.value === 'undefined')||
             (0 > inp.value)||
             (
-                (bars[inp.bar].max !== 'auto')&&
+                (_bars[inp.bar].max !== 'auto')&&
                 (inp.value > bars[inp.bar].max)
             )
         )
             inp.value = 0;
         if(typeof inp.color === 'undefined')
             inp.color = 'white';
-        if(inp.title.toString().length+2 > bars[inp.bar].size)
-            bars[inp.bar].size = inp.title.length+2;
-        bars[inp.bar].lines[inp.id] = {
+        if(inp.title.toString().length+2 > _bars[inp.bar].size)
+            _bars[inp.bar].size = inp.title.length+2;
+        _bars[inp.bar].lines[inp.id] = {
             title:inp.title,
             color:inp.color,
             value:inp.value
         };
         return true;
     };
-    const upDate = function(inp){
+    const _upDate = function(inp){
         if(
-            (typeof bars[inp.name] === 'undefined')||
-            (typeof bars[inp.name].lines === 'undefined')
+            (typeof _bars[inp.name] === 'undefined')||
+            (typeof _bars[inp.name].lines === 'undefined')
         )
             return false;
         for(let i in inp.update){
-            if(typeof bars[inp.name].lines[i] === 'undefined')
+            if(typeof _bars[inp.name].lines[i] === 'undefined')
                 continue;
-            bars[inp.name].lines[i].value = inp.update[i];
+            _bars[inp.name].lines[i].value = inp.update[i];
         }
         return true;
     };
-    const draw = function(bar){
+    const _draw = function(bar){
         if(
             (typeof bar === 'undefined')||
-                 (typeof bars[bar] === 'undefined')
+            (typeof _bars[bar] === 'undefined')
         )
             return false;
-        let siz = bars[bar].size,
+        let siz = _bars[bar].size,
             out = '',
-            max = bars[bar].max,
-            lines = bars[bar].lines;
+            max = _bars[bar].max,
+            lines = _bars[bar].lines;
         for (let i in lines){
-            out += title(
-                siz,lines[i].title)+
-                '\u2502'+
-                line(lines[i].color, lines[i].value, max)+
-                '\n';
+            out += (
+                _title(
+                    siz,
+                    lines[i].title
+                )+' \u2502'+
+                 _line(
+                    lines[i].color,
+                    lines[i].value, 
+                    max
+                )+'\n'
+            );
         }
         return (out);
     };
-    const title = function(size,title){
-        let spaces = size - title.toString().length-1,
-            ot = '';
-        for (let i = 0; spaces > i ; i++)
-            ot += ' ';
-        return ot+title+' ';
+    const _title = function(size,title){
+        return title.padStart(
+            size,
+            ' '
+        );
     };
-    const line = function(color,size,max){
-        let siz = Math.floor(size*(35/max)),
-            ut = '';
-        for (let i =1 ; 35>i;i++)
-            if(i>siz)
-                ut +=' ';
-            else
-                ut +='\u2588';
-        return styler.style(ut, [{color}]);
+    const _line = function(color,size,max){
+        const siz = Math.floor(size*(35/max));
+        return (
+            styler.style(
+                ('').padStart(
+                    siz,
+                    '█'
+                ),
+                [{color}]
+            )+styler.style(
+                ('').padStart(
+                    (35-siz),
+                    ' '
+                ),
+            )
+        );
     };
 };
 
