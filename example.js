610e712c82cc444b66f0cf2fcc863100bcf5a2d1
Soldy <4786022+Soldy@users.noreply.github.com>
2021-04-18 10:58:17 +0100

    github ci fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -1,28 +1,24 @@
-const bar = new (require('./index.js')).base();
+const bar = new (require('./index.js')).base({
+   'name':'testbar',
+   'max' : 40
+});
 const stdio = new (require('consolestdiorc')).base();
 const waittime = 10; // sec
 const w = waittime * 1000;
 
-bar.init({
-   'name':'testbar',
-   'max' : 40
-})
-bar.addLine({
-    'bar'   : 'testbar',
+bar.add({
     'id'    : '1',
     'title'  : 'test bar blue',
     'color' : 'blue'
     }
 );
-bar.addLine({
-    'bar'   : 'testbar',
+bar.add({
     'id'    : '2',
     'title' : 'test bar green',
     'color' : 'green'
     }
 );
-bar.addLine({
-    'bar'   : 'testbar',
+bar.add({
     'id'    : '3',
     'title' : 'test bar red',
     'color' : 'red'
@@ -30,6 +26,7 @@ bar.addLine({
 );
 stdio.cursorHide();
 let randomBarI = 0;
+
 function randomBar(){
     bar.update({
         'name'   : 'testbar',
@@ -41,12 +38,12 @@ function randomBar(){
     stdio.clear();
     stdio.cursorUp(3);
     stdio.print(
-         bar.draw('testbar')
+         bar.draw()
     );
     randomBarI++;
     if(randomBarI > 100){
         stdio.cursorShow();
-        process.exit(1);
+        process.exit(0);
     }
     setTimeout(randomBar,100)
 }
