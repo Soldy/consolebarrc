491ec5f6414d7f1b9d7384c326e7576ff6398266
Soldy <4786022+Soldy@users.noreply.github.com>
2021-09-25 23:21:45 +0100

    example fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -1,48 +1,48 @@
-const bar = new (require('./index.js')).base({
+const $bar = new (require('./index.js')).base({
    'name':'testbar',
    'max' : 40
 });
-const stdio = new (require('consolestdiorc')).base();
-const waittime = 10; // sec
-const w = waittime * 1000;
+const $stdio = new (require('consolestdiorc')).base();
+const _wait_time = 10; // sec
+const _w = _wait_time * 1000;
 
-bar.add({
+$bar.add({
     'id'    : '1',
     'title'  : 'test bar blue',
     'color' : 'blue'
     }
 );
-bar.add({
+$bar.add({
     'id'    : '2',
     'title' : 'test bar green',
     'color' : 'green'
     }
 );
-bar.add({
+$bar.add({
     'id'    : '3',
     'title' : 'test bar red',
     'color' : 'red'
     }
 );
-stdio.cursorHide();
-let randomBarI = 0;
+$stdio.cursorHide();
+let _randomBarI = 0;
 
 function randomBar(){
-    bar.update({
+    $bar.update({
         'name'   : 'testbar',
         'update' : {
             '1' : Math.floor(Math.random()*40),
             '2' : Math.floor(Math.random()*40),
             '3' : Math.floor(Math.random()*40)
         }});
-    stdio.clear();
-    stdio.cursorUp(3);
-    stdio.print(
-         bar.draw()
+    $stdio.clear();
+    $stdio.cursorUp(3);
+    $stdio.print(
+         $bar.draw()
     );
-    randomBarI++;
-    if(randomBarI > 100){
-        stdio.cursorShow();
+    _randomBarI++;
+    if(_randomBarI > 100){
+        $stdio.cursorShow();
         process.exit(0);
     }
     setTimeout(randomBar,100)
